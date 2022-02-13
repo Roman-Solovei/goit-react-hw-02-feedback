ac57550071f6d1c93a4a2a3508d9bb9918069f5d
Roman-Solovei <romario2409@gmail.com>
2022-02-13 20:29:06 +0200

    hw 02 01

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from 'components/App';
+import App  from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />    
   </React.StrictMode>,
   document.getElementById('root')
 );
